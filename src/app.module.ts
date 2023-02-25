a29deae0e9541dde330531b8a6597cfd3a4f685c
Khalid Abbas <khalidabbas.dev@gmail.com>
2023-02-26 03:38:33 +0500

    Added typeorm configurations and database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { TweetsModule } from './modules/tweets/tweets.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './modules/auth/constants';
+import { AuthService } from './modules/auth/auth.service';
+import { UsersService } from './modules/users/users.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './modules/database/database.module';
+import databaseOptions from './config/db.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseOptions],
+      envFilePath: [`${process.env.NODE_ENV}.env`],
+    }),
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
